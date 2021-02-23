c43e1baea8d85a5ade6ef098d1ba870dbb3598c4
YoussefCheikhani <youssef.cheikhani@gmail.com>
2021-02-23 01:20:00 +0100

    adding useState & handling click

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>
+
+    <Folder name="Desktop">
+      <Folder name="Grand Theft Auto V">
+        <File name="GTA.exe"/>
+        <File name="settings.xml"/> 
+      </Folder>
+      <File name="Microsoft Edge.exe"/>
+      <File name="Photo.png"/>
+    </Folder>
+    <Folder name="Applications"/>
+    
+
+  </div>;
 }
+
+const Folder =(props) =>{
+  const [isOpen, setIsOpen] = useState(false);
+  const {name, children} = props;
+
+  const handleClick = () =>{
+    setIsOpen(!isOpen);
+  }
+
+  return <div>
+    <span onClick={handleClick}>{name}</span>     {/* {function} pass the entire function as a prop. {function()} pass the return value of the function */}
+    <div style={{marginLeft:'20px'}}>
+      {isOpen ? children : null}
+    </div>
+  </div>;
+}
+
+const File = (props)=>{
+  return <div>{props.name}</div>;
+}
+
 
 export default App;
